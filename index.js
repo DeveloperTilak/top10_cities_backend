ecee4b6721ccf1ce179f09fd3d1226423f44f4d2
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-09 12:00:54 +0530

    post method successfully completed

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require("express");
 const { connect } = require("./config/db");
+const { CityModel } = require("./model/description.model");
 
 const app = express();
-app.use(express.json())
-
+app.use(express.json());
 
 app.get("/cities", (req, res) => {
   res.send("This is homepage.");
 });
 
-
-app.post("/cities/add", async(req, res)=>{
-    
+app.post("/cities/add", async (req, res) => {
+  try {
     console.log(req.body);
-    res.send("added")
-    
-})
+    const new_city = new CityModel(req.body);
 
+    await new_city.save();
 
+    res.status(200).send("New city added successfully.");
+  } catch (error) {
+    console.log("Error while adding the data into db.");
+    res
+      .status(500)
+      .send({ message: "Error while adding the data into db.", error: error });
+  }
+});
 
-
-
-app.use( (req, res)=>{
-    res.status(404).send("The URL or Method, not exist Please try again.")
-})
-
+app.use((req, res) => {
+  res.status(404).send("The URL or Method, not exist Please try again.");
+});
 
 let PORT = 3000;
 app.listen(PORT, async () => {
