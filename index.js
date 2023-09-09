75007a4c8fb3ac09c4745510a963cf4bb8d084e8
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-09 14:35:14 +0530

    added new one

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 
-const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:5173/'];
+const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:5173/','https://benevolent-ganache-50967c.netlify.app/'];
 
 const corsOptions = {
   origin: allowedOrigins,
